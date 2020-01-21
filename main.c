2ad926666c014d94c10baf358cce8c63571efa80
tomluxton <thomas.luxton@yahoo.com.au>
2020-01-21 20:55:23 +1000

    Optimised UX

--- main.c
+++ main.c
@@ -13,30 +13,28 @@ int main()
     float week_meals;
     float pay;
 
-    printf("How many hours did you work on Saturday? (including breaks)\n");
+    printf("How many hours did you work on Saturday? (including all breaks)\n");
     scanf("%f", &sat_hours);
-    printf("How many hours did you work on Sunday? (including breaks)\n");
+    printf("How many hours did you work on Sunday? (including all breaks)\n");
     scanf("%f", &sun_hours);
 
-    printf("How many hours did you work on Weekdays \n(including paid and unpaid breaks)?\n");
+    printf("How many hours did you work on Weekdays (including allbreaks)?\n");
     scanf("%f", &week_hours);
 
-    if (sat_hours != 0) {
-        printf("How many Saturday shifts were over 5 hours \n(How many unpaid 30mins did you take)?\n");
-        scanf("%f", &sat_meals);
+    if (sat_hours >= 5) {
+        sat_meals = 1;
     } else {
         sat_meals = 0;
     }
 
-    if (sun_hours != 0) {
-        printf("How many Sunday shifts were over 5 hours \n(How many unpaid 30mins did you take)?\n");
-        scanf("%f", &sun_meals);
+    if (sun_hours >= 5) {
+        sun_meals = 1;
     } else {
         sun_meals = 0;
     }
 
-    if (week_hours != 0) {
-        printf("How many Weekday shifts were over 5 hours \n(How many unpaid 30mins did you take)?\n");
+    if (week_hours >= 5) {
+        printf("How many Weekday shifts were 5 or more hours \n(How many unpaid 30mins did you take)?\n");
         scanf("%f", &week_meals);
     } else {
         week_hours = 0;
@@ -44,6 +42,6 @@ int main()
 
     pay = ((sat_hours - (sat_meals * 0.5)) * 1.25 + (sun_hours - (sun_meals * 0.5)) * 1.5 + week_hours - week_meals * 0.5) * BASE;
 
-    printf("Your pay for the week is:%f\n", pay);
+    printf("Your pay for the week is: $%f\n", pay);
     return 0;
 }
